cec08beb14649b2a736e426b011ab5bd8633f5af
Anderson Matheus Arruda <andmarruda@gmail.com>
2021-11-14 09:17:29 -0300

    Update proftpd_users.sql

--- sql/postgresql/proftpd_users.sql
+++ sql/postgresql/proftpd_users.sql
@@ -1,11 +1,11 @@
 CREATE TABLE proftpd_users(
 	id_proftpd_users SERIAL NOT NULL CONSTRAINT proftpd_users_pkey PRIMARY KEY,
-	username varchar(100) not null,
-	pass varchar(32) not null,
-	uid varchar(5) not null,
-	gid varchar(5) not null,
+	username character varying(100) not null,
+	pass character varying(32) not null,
+	uid character varying(5) not null,
+	gid character varying(5) not null,
 	homedir text not null,
-	shell varchar(20) not null default '/bin/false',
+	shell character varying(20) not null default '/bin/false',
 	pub_key character varying(8192) not null
 ) with(
 oids=false
